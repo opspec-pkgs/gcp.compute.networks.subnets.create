66b46e6aa179c318246cf8f57ffed8a206b532c0
Chris Dostert <chris.dostert@live.com>
2019-01-28 17:14:16 -0800

    initial commit

---
+++ cmd.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+gcloud auth activate-service-account --key-file=/keyFile
+
+echo "checking for existing subnet"
+
+if eval "gcloud compute networks describe $name --project $projectId" >/dev/null 2>&1
+then
+  echo "found exiting subnet"
+  exit
+else
+  echo "existing subnet not found"
+fi
+
+echo "creating subnet..."
+subnetCreateCmd="gcloud compute networks subnets create $name"
+subnetCreateCmd=$(printf "%s --network %s" "$subnetCreateCmd" "$network")
+subnetCreateCmd=$(printf "%s --project %s" "$subnetCreateCmd" "$projectId")
+subnetCreateCmd=$(printf "%s --range %s" "$subnetCreateCmd" "$range")
+
+# handle opts
+if [ "$enableFlowLogs" = "true" ]; then
+  clusterCreateCmd=$(printf "%s --enable-flow-logs" "$enableFlowLogs")
+fi
+if [ "$enablePrivateIpGoogleAccess" = "true" ]; then
+  clusterCreateCmd=$(printf "%s --enable-private-ip-google-access" "$enablePrivateIpGoogleAccess")
+fi
+if [ "$description" != " " ]; then
+  subnetCreateCmd=$(printf "%s --description" "$description")
+fi
+if [ "$secondaryRange" != " " ]; then
+  subnetCreateCmd=$(printf "%s --secondary-range" "$secondaryRange")
+fi
+
+eval "$subnetCreateCmd"
